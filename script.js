81d193f81eeef515686a3a137ec7a12060954f2f
rodrigues123321 <wanderson.r.reis@aluno.senai.br>
2025-11-28 17:05:43 -0300

    segundo comandoooooo

---
+++ script.js
@@ -0,0 +1,84 @@
+// let nome = "João";
+// let idade = 25;
+// let estalogado = "São Paulo";
+
+// let ola = "Olá, Seu nome!"
+
+// console.log(ola);
+
+// var mome = "Maria";
+// let idadu = 30;
+// const pi = 3.14;
+// let noma = "Carlos";
+// let mensagem = "Bem-vindo ao site!";
+
+// let idada = 28;
+// let preco = 19.99;
+
+// let ativo = true;
+// let admin = false;
+
+// let frutas = ["maçã", "banana", "laranja"];
+
+// let pessoa = {
+//     nome: "Ana",
+//     idade: 22,
+//     cidade: "Rio de Janeiro"
+// };
+// let soma = 10 + 5;
+// console.log("A soma:"
+// , soma);
+// let subtracao = 10 - 2;
+// console.log("A subtração: ",  subtracao);
+// let divisao = 10 / 2;
+// console.log("A divisão: ", divisao);
+// let multiplicacao = 10 * 2;
+// console.log("A multiplicação: ", multiplicacao);
+// 
+// let e = true && false;let igualdade = 10 == 10;
+// let estritamenteIgual = 10 === "10";
+// let diferente = 10 != 5;
+// let maiorQue = 10 > 5;
+// let menorQue = 5 < 10;
+// console.log("Igualdade: ", igualdade);
+// console.log("Estritamente Igual: ", estritamenteIgual);
+// console.log("Diferente: ", diferente);
+// console.log("Maior Que: ", maiorQue);
+// console.log("Menor Que: ", menorQue);
+
+// let ou = true || false;
+// let e = true && true;
+// let nao = !true;
+// console.log("E lógico: ", e);
+// console.log("OU lógico: ", ou);
+// console.log("NÃO lógico: ", nao);
+
+// let idade = 16;
+// if (idade <= 15) {
+//     // 16 >= 15
+//     console.log("Você é maior de idade.");
+// } else {
+//     console.log("Você é menor de idade.");
+// }
+// let idade = 20;
+// if (idade < 18) {
+//     console.log("Você é maior de idade.");
+// } else if (idade >15) {
+//     console.log("adolescente");
+// } else {
+//     console.log("Você é menor de idade.");
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Número: ", + i);
+// }
+// let contador = 1;
+// while (contador <= 5) {
+//     console.log("Contador: " + contador);
+//     contador++;
+// }
+function saudacao(nome) {
+    return "Olá, " + nome + "!";
+}
+let mensagem = saudacao("Pedro");
+console.log(mensagem);
